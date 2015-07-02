188b7df1e432324334ffef2b977408919657db1c
Michael Topping <mtopping@astro.ucla.edu>
2015-07-02 12:21:28 -0700

    Added Devin Simulation

---
+++ devinsbigfatrooster.py
@@ -0,0 +1,17 @@
+import sys
+DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkK=range
+DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK=sys.stdout
+import time
+DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkv=time.sleep
+ii=0
+for DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvk in DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkK(78):
+ DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkv(0.1)
+ DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.write("\r8"+"="*DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvk+"D")
+ DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.flush()
+ ii+=1
+DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.write("\r"+"8"+"="*74+"...")
+DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.flush()
+while 1:
+ pass
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
+
