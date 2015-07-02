e897eac0fb0fa47daaf11b2912767b80b411e9a1
Michael Topping <mtopping@astro.ucla.edu>
2015-07-02 14:00:07 -0700

    Added Bathmat's suggestion

--- devinsbigfatrooster.py
+++ devinsbigfatrooster.py
@@ -1,17 +1,25 @@
 import sys
-DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkK=range
-DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK=sys.stdout
+import subprocess
 import time
-DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkv=time.sleep
+import os
 ii=0
-for DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvk in DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkK(78):
- DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAkv(0.1)
- DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.write("\r8"+"="*DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvk+"D")
- DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.flush()
- ii+=1
-DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.write("\r"+"8"+"="*74+"...")
-DqthUHJndfFeQRXGOLjCoxmyTcBbsNIWSEMPwgVuaYrzilpAvK.flush()
+length = 0
 while 1:
- pass
-# Created by pyminifier (https://github.com/liftoff/pyminifier)
-
+	length += 1
+	f = open("ssdfsdfsdfwerwerwersdfsdfsdxcvxvxcvxcvsdfsdfsdfsdfs.sh", "w")
+	f.write("#!/bin/sh\n")
+	rows, columns = os.popen('stty size', 'r').read().split()
+	if length > int(columns):
+		if length == int(columns)+2:
+			os.remove('ssdfsdfsdfwerwerwersdfsdfsdxcvxvxcvxcvsdfsdfsdfsdfs.sh')
+			exit()
+		command = "printf "+' "\e[8;2;'+str(length)+';t" \n'
+	else: 
+		command = "printf "+' "\e[8;2;'+columns+';t" \n'
+	f.write(command)
+	f.close()
+	os.chmod('./ssdfsdfsdfwerwerwersdfsdfsdxcvxvxcvxcvsdfsdfsdfsdfs.sh', 0777)
+	subprocess.call(['./ssdfsdfsdfwerwerwersdfsdfsdxcvxvxcvxcvsdfsdfsdfsdfs.sh'])
+	sys.stdout.write("\r8"+"="*(length-2)+"D")
+	sys.stdout.flush()
+	time.sleep(0.1)
